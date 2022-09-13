beeaf26ce3c026c95064700cee744acfe131ba6f
rutik <90312737+Rutikrtr@users.noreply.github.com>
2022-09-13 20:56:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calc(){
+    var n1 =document.getElementById('n1').value;
+    var n2 =document.getElementById('n2').value;
+    var n3 =document.getElementById('n3').value;
+
+    document.getElementById('result').value=(n1/n2)*n3;
+};
+
+
+// let btnClear = document.querySelector('button');
+// let inputs = document.querySelectorAll('input');
+ 
+
+// btnClear.addEventListener('click', () => {
+//     inputs.forEach(input =>  input.value = '');
+// });
+
+
+
+
